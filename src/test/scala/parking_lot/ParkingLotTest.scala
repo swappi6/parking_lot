37c34598a810648d271415e4597f0b04532fd329
swapnil <swapnil@zilingo.com>
2021-02-03 13:55:04 +0530

    Added some more unit tests

--- src/test/scala/parking_lot/ParkingLotTest.scala
+++ src/test/scala/parking_lot/ParkingLotTest.scala
@@ -1,8 +1,8 @@
 package parking_lot
 
 import org.scalatest.FunSuite
-import parking_lot.models.ParkingLotResponseDTOs.ParkingLotStatus
-import parking_lot.models.{FailureResponse, SuccessResponse}
+import parking_lot.models.ParkingLotResponseDTOs.{ParkedVehicleSlotResponse, ParkedVehicleSlotsResponse, ParkedVehiclesResponse, ParkingLotStatus}
+import parking_lot.models.{Color, FailureResponse, SuccessResponse}
 
 class ParkingLotTest extends FunSuite with CommandLineInterceptor {
 
@@ -46,8 +46,72 @@ class ParkingLotTest extends FunSuite with CommandLineInterceptor {
     CommandProcessor("park KA-01-HH-3143 Black")
     CommandProcessor("park KA-01-HH-3144 White")
 
+    val correctOutput = Map(
+      1 -> ("KA-01-HH-3141", "Black"),
+      2 -> ("KA-01-HH-3142", "Blue"),
+      3 -> ("KA-01-HH-3143", "Black"),
+      4 -> ("KA-01-HH-3144", "White")
+    )
+
     assert(CommandProcessor("status") match {
-      case p: SuccessResponse[ParkingLotStatus @unchecked] => p.res.occupiedParkingSlots.size == 4
+      case p: SuccessResponse[ParkingLotStatus @unchecked] => p.res.occupiedParkingSlots.size == 4 &&
+        p.res.occupiedParkingSlots.zipWithIndex.forall {
+          case (slot, idx) => correctOutput.get(idx+1).exists {
+            case (regNum, color) => slot.vehicle.registrationNum == regNum &&
+              slot.vehicle.color.asString == Color.fromString(color).asString
+          }
+        }
+      case _: FailureResponse[_] => false
+    })
+  }
+
+  test("Should show registration numbers of cars with given color") {
+    parkingLotService.doCleanup()
+    CommandProcessor("create_parking_lot 6")
+    CommandProcessor("park KA-01-HH-3141 Black")
+    CommandProcessor("park KA-01-HH-3142 Blue")
+    CommandProcessor("park KA-01-HH-3143 Black")
+    CommandProcessor("park KA-01-HH-3144 White")
+
+    val correctOutput = Map(1 -> "KA-01-HH-3141", 2 -> "KA-01-HH-3143")
+
+    assert(CommandProcessor("registration_numbers_for_cars_with_colour Black") match {
+      case p: SuccessResponse[ParkedVehiclesResponse @unchecked] =>
+        p.res.vehicles.size == 2 & p.res.vehicles.zipWithIndex.forall {
+          case (v, idx) => correctOutput.get(idx + 1).contains(v.registrationNum)
+        }
+      case f: FailureResponse[_] => false
+    })
+  }
+
+  test("Should show slot number of cars with given registration num") {
+    parkingLotService.doCleanup()
+    CommandProcessor("create_parking_lot 6")
+    CommandProcessor("park KA-01-HH-3141 Black")
+    CommandProcessor("park KA-01-HH-3142 Blue")
+    CommandProcessor("park KA-01-HH-3143 Black")
+    CommandProcessor("park KA-01-HH-3144 White")
+
+    assert(CommandProcessor("slot_number_for_car_with_registration_number KA-01-HH-3141") match {
+      case p: SuccessResponse[ParkedVehicleSlotResponse @unchecked] => p.res.slotId == 1
+      case _: FailureResponse[_] => false
+    })
+  }
+
+  test("Should show slot numbers of cars with given color") {
+    parkingLotService.doCleanup()
+    CommandProcessor("create_parking_lot 6")
+    CommandProcessor("park KA-01-HH-3141 Black")
+    CommandProcessor("park KA-01-HH-3142 Blue")
+    CommandProcessor("park KA-01-HH-3143 Black")
+    CommandProcessor("park KA-01-HH-3144 White")
+
+    val correctOutput = Map(1 -> 1, 2 -> 3)
+
+    assert(CommandProcessor("slot_numbers_for_cars_with_colour Black") match {
+      case p: SuccessResponse[ParkedVehicleSlotsResponse @unchecked] => p.res.slots.zipWithIndex.forall {
+        case (slotId, idx) => correctOutput.get(idx+1).contains(slotId)
+      }
       case _: FailureResponse[_] => false
     })
   }
